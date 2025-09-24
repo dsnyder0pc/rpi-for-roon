2bff268bc43e76173e34a5c52e364f6cdbdab91f
dsnyder0pc <dsnyder0cnn@gmail.com>
2025-09-24 10:03:05 -0700

    qa8

--- scripts/diretta-target-checks.sh
+++ scripts/diretta-target-checks.sh
@@ -4,12 +4,12 @@
 #
 
 # --- Colors and Formatting ---
-C_RESET='\033[0m'
-C_RED='\033[0;31m'
-C_GREEN='\033[0;32m'
-C_YELLOW='\033[1;33m'
-C_BLUE='\033[0;34m'
-C_BOLD='\033[1m'
+C_RESET=$'\033[0m'
+C_RED=$'\033[0;31m'
+C_GREEN=$'\033[0;32m'
+C_YELLOW=$'\033[1;33m'
+C_BLUE=$'\033[0;34m'
+C_BOLD=$'\033[1m'
 
 # --- Helper Functions ---
 check() {
