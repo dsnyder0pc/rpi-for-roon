6356c4399310ac43acdf99d674c04f1c9365cb0e
dsnyder0pc <dsnyder0cnn@gmail.com>
2026-05-18 00:25:54 -0700

    tqax

--- scripts/diretta-target-checks.sh
+++ scripts/diretta-target-checks.sh
@@ -252,7 +252,7 @@ check "'diretta-alsa-target' is installed" "[ -d /opt/diretta-alsa-target ]"
 check "'diretta_alsa_target' service is enabled" "systemctl is-enabled diretta_alsa_target.service"
 check "'diretta_alsa_target' service is active" "systemctl is-active diretta_alsa_target.service"
 check "USB DAC/DDC is configured and detected" "grep -q ' \[.*\]:' /proc/asound/cards"
-check_status "Diretta Target License Status" "ls /opt/diretta-alsa-target/ | grep -qv '^diretta'" "activated" "limited"
+check_status "Diretta Target License Status" "grep -q '^Licensed' /tmp/diretta_license_url.cache" "activated" "limited"
 
 header "Section 8a" "Diretta Compiler Toolchain"
 # --- Version-Aware Compiler Checks ---
